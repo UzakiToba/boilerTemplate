b72d7f8c0cc41f81f3f2eca887cd0e2a7aadd086
UzakiToba <yukiuzu0912@gmail.com>
2019-05-18 16:11:05 +0900

    コメント追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// ファイルパスの操作
 const path = require('path');
 const webpack = require('webpack');
 // エラー表示を綺麗に
@@ -5,7 +6,9 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 // ファイルパスの大文字と小文字を区別できるようにする
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
+// ソースが置いてあるフォルダー
 const SRC_ROOT = path.join(__dirname, 'src');
+// 書き出し先フォルダー
 const BUILD_ROOT = path.resolve(__dirname, 'dist');
 
 module.exports = {
@@ -17,8 +20,9 @@ module.exports = {
   },
   // 書きだし設定
   output: {
+    // 書き出しファイル名
     filename: '[name].js',
-    publicPath: '/',
+    // 書きだしフォルダー
     path: BUILD_ROOT,
     // ライブラリの作り方による差異を吸収
     globalObject: 'this'
@@ -26,12 +30,15 @@ module.exports = {
   module: {
     rules: [
       {
+        // 読み込みファイルの正規表現
         test: /\.*js$/,
+        // 除外ファイル
         exclude: /node_modules/,
         use: 'babel-loader'
       },
       // eslintでコンパイル前にコードをチェック
       {
+        // 事前実行
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
@@ -46,11 +53,13 @@ module.exports = {
     extensions: ['.js', '.jsx', '.json']
   },
   plugins: [
+    // エラー表示を綺麗に
     new FriendlyErrorsWebpackPlugin(),
+    // ファイルパスの大文字と小文字を区別できるようにする
     new CaseSensitivePathsPlugin(),
     // コンパイルの経過を表示
     new webpack.ProgressPlugin(),
-    //moment.jsを使用する場合enと、ここで指定しているja以外を除去する
+    // moment.jsを使用する場合enと、ここで指定しているja以外を除去する
     new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ja/)
   ]
 };
