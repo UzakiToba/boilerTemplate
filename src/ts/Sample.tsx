3d244263bf438e3bc94647094d22c4797a058174
UzakiToba <yukiuzu0912@gmail.com>
2019-05-19 22:37:22 +0900

    ソースが変わったのを戻す

--- src/ts/Sample.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-const Sample: React.FC = (): JSX.Element => {
-    return <p>Sample Component!!!</p>;
-};
-
-export default Sample;
--- src/ts/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-
-// component
-import Sample from './Sample';
-
-ReactDOM.render(<Sample />, document.getElementById('app'));
